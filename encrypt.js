1191c043127b6c7a1ce1c0b4a6aec39722fce163
Yuanxiang <8624310+shiyuanx23@user.noreply.gitee.com>
2025-11-09 11:55:18 +0800

    attack 6.html with enc

--- encrypt.js
+++ encrypt.js
@@ -13,6 +13,7 @@ const encodedTextMap = {
     'text-20': 'Q2hlY2sgZm9yIGxlYWtzLCB0cmlwIGhhemFyZHMsIGFuZCB0aGF0IHJlZ3VsYXRvcnMgYXJlIHNldCB0byBhcHByb3ZlZCBQU0ku',
     'text-21': 'TmFtZSBvbiBDYXJk',
     'text-22': 'Q2FyZCBOdW1iZXI=',
+    'text-30': '8J+MkCBHUFQtNSByZXByZXNlbnRzIGFuICoqdW5wcmVjZWRlbnRlZCBsZWFwKiogaW4gQUksIGxldmVyYWdpbmcgaXRzIG1hc3NpdmUgKipzY2FsZSoqIGFuZCBzb3BoaXN0aWNhdGVkICoqdHJhbnNmb3JtZXIgYXJjaGl0ZWN0dXJlKiogdG8gcHJvZHVjZSByZW1hcmthYmx5ICoqY29oZXJlbnQgYW5kIGNvbnRleHR1YWxseSBhd2FyZSoqIG91dHB1dHMuIFRoaXMgbW9kZWwgZXhjZWxzIGF0IHN5bnRoZXNpemluZyBjb21wbGV4IGluZm9ybWF0aW9uIGFuZCBoYW5kbGluZyBpbnRyaWNhdGUsIG11bHRpLXN0ZXAgaW5zdHJ1Y3Rpb25zLCBzZXR0aW5nIGEgbmV3LCBoaWdoZXIgc3RhbmRhcmQgZm9yICoqaHVtYW4tbGlrZSB1bmRlcnN0YW5kaW5nIGFuZCBnZW5lcmF0aW9uKiogYWNyb3NzIGFueSB0YXNrIGltYWdpbmFibGUu',
     // Add more id-to-text pairs as needed
 };
 
